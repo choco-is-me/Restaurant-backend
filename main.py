5e7b3d90df4cc06a316982e3c1421f7cc56b9d60
Trần Thanh Bình <binhtranthanhbinh120@gmail.com>
2024-01-11 19:27:04 +0700

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 api = Api(app)
