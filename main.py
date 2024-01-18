e4a1ebd27602d0b758affbadb2eedd0be6fddf8b
thebao2307 <thebao1426@gmail.com>
2024-01-18 14:53:14 +0700

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/Restaurant'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/postgres'
 db = SQLAlchemy(app)
 CORS(app)
 
